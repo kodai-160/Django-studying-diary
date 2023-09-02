a13a2fecc2bbc1e5ad0bf4f124166031993eb2e6
yoshiyuki-140 <c1221251@st.kanazawa-it.ac.jp>
2023-09-02 10:48:49 +0900

    I developed view for printing 'Hello World

--- djangosnippets/snippets/tests.py
+++ djangosnippets/snippets/tests.py
@@ -1,3 +1,19 @@
+from django.http import HttpRequest
 from django.test import TestCase
+from snippets.views import top  # これから実装するビュー (このコードを書き始めた時点ではまだ存在しない)
+
 
 # Create your tests here.
+
+class TopPageViewTest(TestCase):
+
+    def test_top_returns_200(self):
+        request = HttpRequest()
+        response = top(request)
+        self.assertEqual(response.status_code, 200)
+
+    def test_top_returns_expected_content(self):
+        pass
+        request = HttpRequest()
+        response = top(request)
+        self.assertEqual(response.content, b"Hello World")
