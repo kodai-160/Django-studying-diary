6a8afd536f231ccfa969a8e822ddd5141882d86b
yoshiyuki-140 <c1221251@st.kanazawa-it.ac.jp>
2023-09-02 10:59:07 +0900

    1.2.3のHello Worldの表示(ルーティングの設定)

--- djangosnippets/snippets/tests.py
+++ djangosnippets/snippets/tests.py
@@ -1,19 +1,13 @@
-from django.http import HttpRequest
 from django.test import TestCase
-from snippets.views import top  # これから実装するビュー (このコードを書き始めた時点ではまだ存在しない)
 
 
 # Create your tests here.
 
-class TopPageViewTest(TestCase):
-
+class TopPageTest(TestCase):
     def test_top_returns_200(self):
-        request = HttpRequest()
-        response = top(request)
+        response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
 
     def test_top_returns_expected_content(self):
-        pass
-        request = HttpRequest()
-        response = top(request)
+        response = self.client.get("/")
         self.assertEqual(response.content, b"Hello World")
