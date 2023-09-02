0627f63360988a9cc5bc7dd2d5d7cf6881b75fa3
yoshiyuki-140 <c1221251@st.kanazawa-it.ac.jp>
2023-09-02 14:50:35 +0900

    リスト1.26 & 1.28 & 1.29

--- djangosnippets/snippets/tests.py
+++ djangosnippets/snippets/tests.py
@@ -39,28 +39,52 @@ UserModel = get_user_model()
 
 # リスト1.23
 # --------- START -----------
+# class SnippetDetailTest(TestCase):
+# def setUp(self):
+# self.user = UserModel.objects.create(
+# username="test_user",
+# email="test@example.com",
+# password="secret",
+# )
+# self.snippet = Snippet.objects.create(
+# title="タイトル",
+# code="コード",
+# description="解説",
+# created_by=self.user,
+# )
+#
+# def test_should_user_expected_template(self):
+# response = self.client.get(f"/snippets/{self.snippet.id}/")
+# self.assertTemplateUsed(response, "snippets/snippet_detail.html")
+#
+# def test_top_page_returns_200_and_expected_heading(self):
+# response = self.client.get(f"/snippets/{self.snippet.id}/")
+# self.assertContains(response, self.snippet.title, status_code=200)
+# --------- END -----------
 
-
-class SnippetDetailTest(TestCase):
+# リスト1.26
+# --------- START -----------
+class CreateSnippetTest(TestCase):
     def setUp(self):
         self.user = UserModel.objects.create(
             username="test_user",
             email="test@example.com",
             password="secret",
         )
-        self.snippet = Snippet.objects.create(
-            title="タイトル",
-            code="コード",
-            description="解説",
-            created_by=self.user,
-        )
+        self.client.force_login(self.user)  # ユーザーログイン
 
-    def test_should_user_expected_template(self):
-        response = self.client.get(f"/snippets/{self.snippet.id}/")
-        self.assertTemplateUsed(response, "snippets/snippet_detail.html")
+    def test_render_creation_form(self):
+        response = self.client.get("/snippets/new/")
+        self.assertContains(response, "スニペットの登録", status_code=200)
 
-    def test_top_page_returns_200_and_expected_heading(self):
-        response = self.client.get(f"/snippets/{self.snippet.id}/")
-        self.assertContains(response, self.snippet.title, status_code=200)
-
+    def test_create_snippet(self):
+        data = {
+            'title': 'タイトル',
+            'code': 'コード',
+            'description': '解説'
+        }
+        self.client.post("/snippets/new/", data)
+        snippet = Snippet.objects.get(title='タイトル')
+        self.assertEqual('コード', snippet.code)
+        self.assertEqual('解説', snippet.description)
 # --------- END -----------
--- djangosnippets/snippets/views.py
+++ djangosnippets/snippets/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse, HttpResponseForbidden
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 from snippets.models import Snippet
+from snippets.forms import SnippetForm
 
 
 def top(request):
@@ -12,11 +14,32 @@ def top(request):
 
 
 def snippet_new(request):
-    return HttpResponse('スニペットの登録')
+    if request.method == "POST":
+        form = SnippetForm(request.POST)
+        if form.is_valid():
+            snippet = form.save(commit=False)
+            snippet.created_by = request.user
+            snippet.save()
+            return redirect(snippet_detail, snippet_id=snippet.pk)
+    else:
+        form = SnippetForm()
+
+    return render(request, "snippets/snippet_new.html", {'form': form})
 
 
 def snippet_edit(request, snippet_id):
-    return HttpResponse('スニペットの編集')
+    snippet = get_object_or_404(Snippet, pk=snippet_id)
+    if snippet.created_by_id != request.user.id:
+        return HttpResponseForbidden("このスニペットの編集は許可されていません")
+    if request.method == "POST":
+        form = SnippetForm(request.POST, instance=snippet)
+        if form.is_valid():
+            form.save()
+            return redirect('snippet_detail', snippet_id=snippet_id)
+
+    else:
+        form = SnippetForm(instance=snippet)
+    return render(request, 'snippets/snippet_edit.html', {'form': form})
 
 
 def snippet_detail(request, snippet_id):
