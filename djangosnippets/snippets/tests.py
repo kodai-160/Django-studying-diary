f3acc74f558a0e81a3c14ea9812aa319b3fc0275
yoshiyuki-140 <c1221251@st.kanazawa-it.ac.jp>
2023-09-02 11:27:56 +0900

    1.2.4ルーティングの設定

--- djangosnippets/snippets/tests.py
+++ djangosnippets/snippets/tests.py
@@ -1,13 +1,20 @@
 from django.test import TestCase
+from django.urls import resolve
+from snippets.views import top, snippet_new, snippet_edit, snippet_detail
 
 
 # Create your tests here.
 
-class TopPageTest(TestCase):
-    def test_top_returns_200(self):
-        response = self.client.get("/")
-        self.assertEqual(response.status_code, 200)
+class CreateSnippetTest(TestCase):
+    def test_should_resolve_snippet(self):
+        found = resolve("/snippets/new/")
+        self.assertEqual(snippet_new,found.func)
+class SnippetDetailTest(TestCase):
+    def test_should_resolve_snippet_detail(self):
+        found = resolve("/snippets/1/")
+        self.assertEqual(snippet_detail,found.func)
 
-    def test_top_returns_expected_content(self):
-        response = self.client.get("/")
-        self.assertEqual(response.content, b"Hello World")
+class EditSnippetTest(TestCase):
+    def test_should_resolve_snippet_edit(self):
+        found = resolve("/snippets/1/edit/")
+        self.assertEqual(snippet_edit,found.func)
