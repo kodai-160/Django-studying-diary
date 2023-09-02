6204e49a45852f788403d7c041f4010046b198a8
yoshiyuki-140 <c1221251@st.kanazawa-it.ac.jp>
2023-09-02 13:11:35 +0900

    リスト1.15

--- djangosnippets/snippets/tests.py
+++ djangosnippets/snippets/tests.py
@@ -1,20 +1,16 @@
 from django.test import TestCase
-from django.urls import resolve
-from snippets.views import top, snippet_new, snippet_edit, snippet_detail
 
 
 # Create your tests here.
 
-class CreateSnippetTest(TestCase):
-    def test_should_resolve_snippet(self):
-        found = resolve("/snippets/new/")
-        self.assertEqual(snippet_new,found.func)
-class SnippetDetailTest(TestCase):
-    def test_should_resolve_snippet_detail(self):
-        found = resolve("/snippets/1/")
-        self.assertEqual(snippet_detail,found.func)
+# トップページがb"Hello Wrold"というプレーンテキストではなくhtmlファイルを返すように変更するため、
+# TopPageViewTestクラスは削除して、TopPageTestクラスを次んように書き換え
 
-class EditSnippetTest(TestCase):
-    def test_should_resolve_snippet_edit(self):
-        found = resolve("/snippets/1/edit/")
-        self.assertEqual(snippet_edit,found.func)
+class TopPageTest(TestCase):
+    def test_top_page_returns_200_and_expected_title(self):
+        response = self.client.get("/")
+        self.assertContains(response, "Djangoスニペット", status_code=200)
+
+    def test_top_page_uses_expected_template(self):
+        response = self.client.get("/")
+        self.assertTemplateUsed(response, "snippets/top.html")
