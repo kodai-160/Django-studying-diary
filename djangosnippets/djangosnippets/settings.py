5646cd7a12a93eaa555167b29a1baabef59cfe3f
kodai <c1218716@st.kanazawa-it.ac.jp>
2023-09-02 09:47:57 +0900

    change language

--- djangosnippets/djangosnippets/settings.py
+++ djangosnippets/djangosnippets/settings.py
@@ -103,9 +103,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+LANGUAGE_CODE = 'ja'
+
+#   TIME_ZONE = 'UTC'
+
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
