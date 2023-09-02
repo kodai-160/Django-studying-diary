7c98d2f024b2973f1dacac97285b61e2982e3471
yoshiyuki-140 <c1221251@st.kanazawa-it.ac.jp>
2023-09-02 13:43:53 +0900

    リスト1.18

--- djangosnippets/snippets/tests.py
+++ djangosnippets/snippets/tests.py
@@ -1,16 +1,66 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model  # get_user_modelをインポート
+from django.test import TestCase, Client, RequestFactory
+
+from snippets.models import Snippet
+from snippets.views import top
 
 
-# Create your tests here.
+UserModel = get_user_model()
 
-# トップページがb"Hello Wrold"というプレーンテキストではなくhtmlファイルを返すように変更するため、
-# TopPageViewTestクラスは削除して、TopPageTestクラスを次んように書き換え
 
-class TopPageTest(TestCase):
-    def test_top_page_returns_200_and_expected_title(self):
-        response = self.client.get("/")
-        self.assertContains(response, "Djangoスニペット", status_code=200)
+# リスト1.18
+# --------- START -----------
+class TopPageRenderSnippetsTest(TestCase):
+    def setUp(self):
+        self.user = UserModel.objects.create(
+            username="test_user",
+            email="test@example.com",
+            password="top_secret_pass0001",
+        )
+        self.snippet = Snippet.objects.create(
+            title="title1",
+            code="print('hello')",
+            description="description1",
+            created_by=self.user,
+        )
 
-    def test_top_page_uses_expected_template(self):
-        response = self.client.get("/")
-        self.assertTemplateUsed(response, "snippets/top.html")
+    def test_should_return_snippet_title(self):
+        request = RequestFactory().get("/")
+        request.user = self.user
+        response = top(request)
+        self.assertContains(response, self.snippet.title)
+
+    def test_should_return_username(self):
+        request = RequestFactory().get("/")
+        request.user = self.user
+        response = top(request)
+        self.assertContains(response, self.user.username)
+# --------- END -----------
+
+# リスト1.23
+# --------- START -----------
+# 
+# 
+# class SnippetDetailTest(TestCase):
+    # def setUp(self):
+        # self.user = UserModel.objects.create(
+            # username="test_user",
+            # email="test@example.com",
+            # password="secret",
+        # )
+        # self.snippet = Snippet.objects.create(
+            # title="タイトル",
+            # code="コード",
+            # description="解説",
+            # created_by=self.user,
+        # )
+# 
+    # def test_should_user_expected_template(self):
+        # response = self.client.get(f"/snippets/{self.snippet.id}/")
+        # self.assertTemplateUsed(response, "snippets/snippets_detail.html")
+# 
+    # def test_top_page_returns_200_and_expected_heading(self):
+        # response = self.client.get("/snippets/{self.snippets.id}/")
+        # self.assertContains(response, self.snippet.title, status_code=200)
+
+# --------- END -----------
