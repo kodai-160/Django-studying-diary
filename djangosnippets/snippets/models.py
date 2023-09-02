14787f78ffea21e9d33b5a6d63967273de9c3e00
yoshiyuki-140 <c1221251@st.kanazawa-it.ac.jp>
2023-09-02 13:56:27 +0900

    bug fixed

--- djangosnippets/snippets/models.py
+++ djangosnippets/snippets/models.py
@@ -7,7 +7,7 @@ from django.conf import settings
 class Snippet(models.Model):
     title = models.CharField('タイトル', max_length=128)
     code = models.TextField('コード', blank=True)
-    descrption = models.TextField('説明', blank=True)
+    description = models.TextField('説明', blank=True)
     created_by = models.ForeignKey(settings.AUTH_USER_MODEL,
                                    verbose_name="投稿者",
                                    on_delete=models.CASCADE)
