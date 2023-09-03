78df358f51432dc4c08f3f338bad153bbf6961d2
yoshiyuki-140 <c1221251@st.kanazawa-it.ac.jp>
2023-09-03 09:46:25 +0900

    リスト1.32:INSTALLED_APPSにbootstrap5を追加

--- djangosnippets/djangosnippets/settings.py
+++ djangosnippets/djangosnippets/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # installed apps
+    'django_bootstrap5',
 ]
 
 MIDDLEWARE = [
