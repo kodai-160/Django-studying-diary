0373827062e51f62f613714de0717b4a461554e6
kodai-06 <c1218716@st.kanazawa-it.ac.jp>
2023-09-04 11:52:41 +0900

    list1.53

--- djangosnippets/snippets/models.py
+++ djangosnippets/snippets/models.py
@@ -13,6 +13,16 @@ class Snippet(models.Model):
                                    on_delete=models.CASCADE)
     created_at = models.DateTimeField("投稿日", auto_now_add=True)
     updated_at = models.DateTimeField("更新日", auto_now=True)
+    
+
+class Comment(models.Model):
+    text = models.TextField("本文", blank=False)
+    commented_at = models.DateTimeField("投稿日", auto_now_add=True)
+    commented_to = models.ForeignKey(Snippet, verbose_name="スニペット",
+                                     on_delete=models.CASCADE)
+    commented_by = models.ForeignKey(settings.AUTH_USER_MODEL,
+                                     verbose_name="投稿者",
+                                     on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
